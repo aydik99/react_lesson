2603b3bb495963c645bcf6eb90b3a74f6e0046c5
aydik99 <aidemir99@mail.ru>
2018-06-19 12:24:56 +0300

    corrected

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import './Header.css';
+import React, {Component} from 'react';
+import classNames from 'classNames';
+
+export default class Menu extends Component
+{
+    render()
+    {
+        const {size} = this.props;
+        const className = classNames (
+            'header',
+            {
+                'header--small': size === 'small',
+                'header--big': size === 'big'
+            }
+        );
+        return (
+            <div className={className}>
+
+            </div>
+        )
+    }
+}
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap';
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      name: 'Name',
+      email: 'email'
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this); 
+    this.handleChangeEmail = this.handleChangeEmail.bind(this); 
+    this.handleChangeLogin = this.handleChangeLogin.bind(this); 
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+ handleSubmit() {
+  const { onSubmitForm } = this.props;
+    this.setState({
+      modal: !this.state.modal
+    });
+    onSubmitForm(this.state.name, this.state.email);
+  }
+
+  handleChangeEmail(e)
+    {
+
+      const {name} = this.state;
+      var val = e.target.value;
+      this.setState({
+        email: val
+      });
+
+    }
+
+    handleChangeLogin(e) {
+      const {name} = this.state;
+      var val = e.target.value;
+      this.setState({
+        name: val
+      });
+    }
+    
+
+
+  render() {
+    return (
+      <div>
+        <Button color='danger' onClick={this.toggle}>Login</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalBody>
+          <Form>
+        <FormGroup>
+          <Label for='Email'>Email</Label>
+          <Input type='email' name="email" id='exampleEmail'   value={this.state.email}
+          onChange={this.handleChangeEmail}/>
+        </FormGroup>
+        <FormGroup>
+          <Label for='exampleLogin'>Login</Label>
+          <Input type='text' name='login' id='examplelogin'  value={this.state.name}
+          onChange={this.handleChangeLogin}/>
+        </FormGroup>
+        </Form>
+          </ModalBody>
+          <ModalFooter>
+            <Button color='primary' onClick={this.handleSubmit}>Do Something</Button>{' '}
+            <Button color='secondary' onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
